f26f660595a69588ca4358f9ce1a13bcfe7af3c9
Artidek <artyom_84@mail.ru>
2024-10-08 00:24:01 +0200

    final versions after norminette check

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aobshatk <aobshatk@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 22:40:36 by aobshatk          #+#    #+#             */
+/*   Updated: 2024/10/07 22:40:36 by aobshatk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b);
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = temp / *b;
+	*b = temp % *b;
+}
