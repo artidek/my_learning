f369f249bd125eb6be97f6a1e724ceecb4641160
Artidek <artyom_84@mail.ru>
2024-10-17 00:06:12 +0200

    Finished and ready for mini test

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -13,102 +13,70 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_putnbr(int nb)
+int	check_base(char *str)
 {
-	char	c;
+	int		i;
+	int		j;
+	char	check_c;
 
-	c = '0';
-	if (nb < 10 && nb >= 0)
+	i = 0;
+	j = 1;
+	while (str[i])
 	{
-		c = c + nb % 10;
-		write(1, &c, 1);
+		check_c = str[i];
+		while (str[j])
+		{
+			if (check_c == str[j] || str[j] == '-' || str[j] == '+')
+				return (0);
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	return (i);
+}
+
+void	put_nbr(int nbr, char *base, int base_n)
+{
+	int	rem;
+
+	if (nbr < 0 && base_n > 2)
+	{
+		write(1, "-", 1);
+		nbr *= -1;
+	}
+	if (nbr < 0 && base_n == 2)
+	{
+		write(1, "1", 1);
+		nbr *= -1;
+	}
+	if (nbr == 0)
 		return ;
-	}
-	if (nb >= 10)
-	{
-		c = c + nb % 10;
-		ft_putnbr(nb / 10);
-		write(1, &c, 1);
-	}
-	if (nb < 0)
-	{
-		c = '-';
-		write(1, &c, 1);
-		nb = nb * (-1);
-		ft_putnbr(nb / 10);
-		c = '0' + nb % 10;
-		write(1, &c, 1);
-	}
-}
-
-void	put_hex(int n)
-{
-	char	*to_char;
-
-	to_char = "0123456789ABCDEF";
-	if (n < 1)
-		return ;
-	put_hex(n / 16);
-	write(1, &to_char[n % 16], 1);
-}
-
-void	put_bit(unsigned int n)
-{
-	bool zero_fl;
-	char prt_bt;
-	int count;
-
-	zero_fl = false;
-	count = 0;
-	n = (n >> 28) & 0x0F;
-}
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	a;
-	int	b;
-
-	a = 0;
-	b = 0;
-	while (*s1)
-		a += *(s1++);
-	while (*s2)
-		b += *(s2++);
-	if (a == b)
-		return (0);
-	return (1);
+	rem = nbr % base_n;
+	nbr = nbr / base_n;
+	put_nbr(nbr, base, base_n);
+	write(1, &base[rem], 1);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	if (ft_strcmp("0123456789", base) == 0)
-		ft_putnbr(nbr);
-	if (ft_strcmp("01", base) == 0)
-	{
-		if (nbr == 0)
-			write(1, "0", 1);
-		put_bit(nbr);
-	}
-	if (ft_strcmp("0123456789ABCDEF", base) == 0)
-		if (nbr < 0)
-		{
-			write(1, "-", 1);
-			nbr *= -1;
-			put_hex(nbr);
-		}
-	if (ft_strcmp("poneyvif", base) == 0)
-	{
-		if (nbr == 0)
-		{
-			return ;
-		}
-		ft_putnbr_base(nbr / 8, base);
-		ft_putnbr(nbr % 8);
-	}
+	int	base_n;
+
+	base_n = check_base(base);
+	if (base_n == 16)
+		put_nbr(nbr, base, base_n);
+	if (base_n == 10)
+		put_nbr(nbr, base, base_n);
+	if (base_n == 8)
+		put_nbr(nbr, base, base_n);
+	if (base_n == 2)
+		put_nbr(nbr, base, base_n);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	ft_putnbr_base(512, "0123456789");
+	int	result;
+
+	ft_putnbr_base(125, "01");
 	return (0);
-}
+}*/
