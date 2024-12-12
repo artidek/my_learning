d1ac32088961c2e0798ef9ea0c191a29f99f3ba2
Artyom Obshatko <aobshatk@c1r3s2.42warsaw.pl>
2024-12-12 17:57:14 +0100

    upd

--- bonus/ft_lstmap.c
+++ bonus/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aobshatk <aobshatk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 13:34:43 by aobshatk          #+#    #+#             */
-/*   Updated: 2024/12/12 16:37:37 by aobshatk         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:56:32 by aobshatk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,76 +44,73 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*retlst;
 	t_list	*temp;
 	int		size;
-	void *temp_con;
 
 	if (!lst || (!f && !del))
 		return (NULL);
 	retlst = ft_lstnew(lst->content);
 	size = ft_lstsize(lst);
 	temp = retlst;
-	while (size > 0)
+	while (lst->next != NULL)
 	{
 		if (del)
 		{
-			del(temp->content);
-			temp = temp->next;
+			//need to implement delete functionality
+			//change func tested and fine;
 		}
-		temp_con = temp->content;
-		printf("%s\n", (char *)temp->content);
-		temp->content = f(temp_con);
-
-		temp->next = ft_lstnew(lst->next->content);
-		temp = temp->next;
+		temp->content = f(&(lst->content));
 		lst = lst->next;
+		temp->next = ft_lstnew(lst->content);
+		temp = temp->next;
+		//printf("%s\n", (char *)temp->content);
 	}
+	printf("loop finished\n");
 	return (retlst);
 }
 
-static void *func (void *ptr)
+void	*func(void *ptr)
 {
-	void *ret;
+	char	*test;
 
-	printf("finished success!");
-	ret = ptr;
+	test = "fuck";
+	return (test);
+}
 
- 	while((*(char *)ptr))
-	{
-		(*(char *)ptr) += ('z' - 'A');
-		ptr++;
-	}
+void del(void *ptr)
+{
 
-	return (ret);
 }
 
 int	main(void)
 {
-	t_list *orig;
-	t_list *mapped;
-	t_list *temp;
+	t_list	*orig;
+	t_list	*mapped;
+	t_list	*temp;
 
 	orig = ft_lstnew("entry");
 	orig->next = ft_lstnew("second");
 	orig->next->next = ft_lstnew("third");
 	orig->next->next->next = ft_lstnew("forth");
-
 	mapped = ft_lstmap(orig, func, NULL);
-	while(mapped)
+	temp = mapped;
+	while (temp)
 	{
-		printf("%s\n", (char *)mapped->content);
-		mapped = mapped->next;
+		temp  = temp->next;
 	}
 	temp = orig;
-	while(temp)
+	while (temp)
 	{
-		free(temp);
-		orig = orig->next;
-		temp = orig;
+
+		temp = orig->next;
+		free(orig);
+		orig = temp;
 	}
+
 	temp = mapped;
-	while(temp)
+	while (temp)
 	{
-		free(temp);
-		mapped = mapped->next;
-		temp = mapped;
+		printf("freing..\n");
+		temp = mapped->next;
+		free(mapped);
+		mapped = temp;
 	}
 }
